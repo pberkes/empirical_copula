48dae59e0529382bd07edb90601131f14af1e035
Pietro Berkes <pietro.berkes@googlemail.com>
2022-12-04 18:43:32 +0100

    Functions to compute the significance levels under 0-hyp of independence

---
+++ empirical_copula/significance.py
@@ -0,0 +1,98 @@
+import numpy as np
+import pandas as pd
+
+from empirical_copula import joint_counts
+
+
+def _bootstrap_independently(samples, n_bootstraps, random_state=np.random):
+    """ Create datasets with empirical distribution as samples but all dependencies removed.
+
+    The two columns of `samples` are sampled with repetition, independently and with number of
+    samples as in the original dataset. The goal is to create data where each column
+    has the same empirical distribution as the original columns, but the dependencies are
+    gone.
+
+    Parameters
+    ----------
+    samples : DataFrame
+        Pandas DataFrame with two columns, each row representing a sample from two
+        discrete random variables. The dtype of the columns does not matter, but it is expected
+        a number of unique values per column much smaller than the number of samples.
+    n_bootstraps : int
+        Number of bootstrapped datasets.
+    random_state : numpy.RandomState
+        Random number generator. Default is `numpy.random`.
+
+    Returns
+    -------
+    bootstrap_counts : DataFrame
+        A table containing the counts of the joint distribution of the two variables. Each row
+        is a combination of the values of the first and second variable, and each column is a
+        bootstrap resampling.
+
+    """
+    bootstrap_counts = []
+    for _ in range(n_bootstraps):
+        bootstrap_samples = pd.DataFrame(
+            data={
+                'v1': random_state.choice(samples['v1'], size=samples.shape[0], replace=True),
+                'v2': random_state.choice(samples['v2'], size=samples.shape[0], replace=True),
+            }
+        )
+        counts = joint_counts(bootstrap_samples)
+        bootstrap_counts.append(counts.unstack())
+
+    bootstrap_counts = pd.concat(bootstrap_counts, axis=1).fillna(0)
+    return bootstrap_counts
+
+
+def significance_from_bootstrap(samples, n_bootstraps, p_levels_low, random_state=np.random):
+    """ Compute thresholds for significance under the 0-hypothesis of independence.
+
+    Parameters
+    ----------
+    samples : DataFrame
+        Pandas DataFrame with two columns, each row representing a sample from two
+        discrete random variables. The dtype of the columns does not matter, but it is expected
+        a number of unique values per column much smaller than the number of samples.
+    n_bootstraps : int
+        Number of bootstrapped datasets.
+    p_levels_low: list of floats
+        List of significance levels for the low tail, between 0 and 0.5 .
+        The function adds significance levels for the high tail.
+    random_state : numpy.RandomState
+        Random number generator. Default is `numpy.random`.
+
+    Returns
+    -------
+    quantile_levels : list of floats
+        List of significance levels for the low and high tail.
+    quantile_levels_labels : list of int
+        List of labels for the significance levels.
+    significance : DataFrame
+        A table containing the significance label for all combinations of values for the two
+        variables.
+    """
+
+    n_levels = len(p_levels_low)
+    p_levels_high = [1.0 - l for l in reversed(p_levels_low)]
+    quantile_levels = p_levels_low + [0] + p_levels_high
+    quantile_levels_labels = (
+            [-(n_levels - i) for i in range(n_levels)]
+            + [0]
+            + [i+1 for i in range(n_levels)]
+    )
+
+    bootstrap_counts = _bootstrap_independently(samples, n_bootstraps, random_state)
+    bootstrap_quantiles = bootstrap_counts.quantile(quantile_levels, axis=1).stack()
+
+    samples_counts = joint_counts(samples)
+    significance = np.zeros_like(samples_counts)
+    for v, q in zip(quantile_levels_labels, quantile_levels):
+        if v > 0:
+            significance[samples_counts >= bootstrap_quantiles.loc[q]] = v
+        elif v < 0:
+            significance[samples_counts <= bootstrap_quantiles.loc[q]] = v
+    significance = pd.DataFrame(data=significance, index=samples_counts.index, columns=samples_counts.columns)
+
+    return quantile_levels, quantile_levels_labels, significance
---
+++ tests/test_significance.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+
+from empirical_copula.significance import _bootstrap_independently, significance_from_bootstrap
+
+
+def test__bootstrap_independently():
+    samples = pd.DataFrame(
+        data=[['A', 'A', 'B', 'B', 'B'],
+              [100, 200, 100, 300, 500]],
+        index=['v1', 'v2']
+    ).T
+
+    random_state = np.random.RandomState(98)
+    n_bootstraps = 3
+    bootstrap_counts = _bootstrap_independently(samples, n_bootstraps, random_state=random_state)
+
+    # There should be no missing values, those are counts of 0
+    assert (~bootstrap_counts.isnull()).all().all()
+    assert bootstrap_counts.shape[1] == n_bootstraps
+    # only <= because we cannot guarantee that all values are bootstrapped
+    # 2: A, B;  4: 100, 200, 300, 500
+    assert bootstrap_counts.shape[0] <= 2 * 4
+
+
+def test_significance_from_bootstrap():
+    # Marginal distributions are uniform
+    # 'A' and 100 are very dependent (they always appear together)
+    # 'A', 'B' are independent of 200, 300 (they appear equally often together)
+    samples = pd.DataFrame(
+        data=[['A', 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'C', 'C', 'C', 'C'],
+              [100, 100, 100, 100, 200, 300, 200, 300, 200, 300, 200, 300]],
+        index=['v1', 'v2']
+    ).T
+
+    random_state = np.random.RandomState(98)
+    quantile_levels, quantile_levels_labels, significance = significance_from_bootstrap(
+        samples, n_bootstraps=100, p_levels_low=[0.01, 0.1], random_state=random_state)
+
+    assert quantile_levels == [0.01, 0.1, 0, 0.9, 0.99]
+    assert quantile_levels_labels == [-2, -1, 0, 1, 2]
+    assert significance.loc['A', 100] == 2
+    assert (significance.loc['B':'C', 200:300] == 0).all().all()
