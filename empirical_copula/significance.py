Fix signicance computation had hard-coded column names

empirical_copula/significance.py
@@ -31,12 +31,13 @@ def _bootstrap_independently(samples, n_bootstraps, random_state=np.random):
         bootstrap resampling.
 
     """
+    col1, col2 = samples.columns
     bootstrap_counts = []
     for _ in range(n_bootstraps):
         bootstrap_samples = pd.DataFrame(
             data={
-                'v1': random_state.choice(samples['v1'], size=samples.shape[0], replace=True),
-                'v2': random_state.choice(samples['v2'], size=samples.shape[0], replace=True),
+                col1: random_state.choice(samples[col1], size=samples.shape[0], replace=True),
+                col2: random_state.choice(samples[col2], size=samples.shape[0], replace=True),
             }
         )
         counts = joint_counts(bootstrap_samples)

tests/test_significance.py
@@ -8,7 +8,7 @@ def test__bootstrap_independently():
     samples = pd.DataFrame(
         data=[['A', 'A', 'B', 'B', 'B'],
               [100, 200, 100, 300, 500]],
-        index=['v1', 'v2']
+        index=['a', 'b']
     ).T
 
     random_state = np.random.RandomState(98)
@@ -30,7 +30,7 @@ def test_significance_from_bootstrap():
     samples = pd.DataFrame(
         data=[['A', 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'C', 'C', 'C', 'C'],
               [100, 100, 100, 100, 200, 300, 200, 300, 200, 300, 200, 300]],
-        index=['v1', 'v2']
+        index=['c', 'd']
     ).T
 
     random_state = np.random.RandomState(98)